3e85fa67890a9b08c49dc10433b6c7638a324ecd
toyorcee <oluwatoyosiolaniyan@gmail.com>
2025-05-14 19:48:11 +0100

    first push

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <p className="text-red-600 font-semibold">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
